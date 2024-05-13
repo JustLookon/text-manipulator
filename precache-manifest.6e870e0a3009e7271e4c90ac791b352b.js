3bacf73fe505bc135841c0c18efa4366308ec26f
Suraj Singh <surya9112000@gmail.com>
2024-05-13 14:54:58 +0530

    Updates

---
+++ precache-manifest.6e870e0a3009e7271e4c90ac791b352b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7e022ab2c4c9fd20627a38e7b82ab1",
+    "url": "/text-manipulator/index.html"
+  },
+  {
+    "revision": "8a1bed18590deb369958",
+    "url": "/text-manipulator/static/css/main.0591f18a.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/text-manipulator/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/text-manipulator/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "8a1bed18590deb369958",
+    "url": "/text-manipulator/static/js/main.49010fc9.chunk.js"
+  },
+  {
+    "revision": "b9919baa6fed33c966a8",
+    "url": "/text-manipulator/static/js/runtime~main.a3c8f4a8.js"
+  }
+]);
